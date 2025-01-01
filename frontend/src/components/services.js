3e68b7aea119a259cd2dfd09bef9a9e2a9ad8f38
aryanrajput955 <aryan692000@gmail.com>
2025-01-01 23:23:08 +0530

    fixed engineering button

--- frontend/src/components/services.js
+++ frontend/src/components/services.js
@@ -119,24 +119,26 @@ const Services = () => {
             </p>
           </div>
           <div className="px-4 pb-4 pt-0 mt-2">
+            <Link to={"/engineering"}>
             <button
               className="rounded-md border border-transparent py-2 px-4 flex items-center justify-center text-sm transition-all text-blue-500 hover:bg-slate-100 focus:bg-slate-100 hover:text-blue-800 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
-            >
+              >
               Read More
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
                 className="w-4 h-4 ml-1.5"
-              >
+                >
                 <path
                   fillRule="evenodd"
                   d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
                   clipRule="evenodd"
-                />
+                  />
               </svg>
             </button>
+                  </Link>
           </div>
         </div> 
 
